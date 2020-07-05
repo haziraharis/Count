1f7bf36548653b8798b6e76c3cafd411afdf88f7
haziraharis <hazirah.haris@live.iium.edu.my>
2020-07-06 01:20:52 +0800

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      count: 0
+    };
+  //  this.updateCount = this.updateCount.bind(this);
+  //  this.resetCount = this.resetCount.bind(this);
+  }
+/*
+  updateCount(){
+    this.setState({ 
+      count: this.state.count + 1 
+    });
+    console.log(this.state.count);
+  }
+
+  resetCount(){
+    this.setState({
+     count: 0
+    });
+  }
+
+*/
+
+ updateCount = () => this.setState({count: this.state.count + 1});
+ resetCount = () => this.setState({count: 0});
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>{this.state.count}</Text>
+        <Text style={styles.label} onPress={this.updateCount}>Count</Text>
+        <Text style={styles.label} onPress={this.resetCount}>Reset</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  label: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  }
 });
